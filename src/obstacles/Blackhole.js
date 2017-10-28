229d9a6887e2e6daefea08c6d99cd3b9f171bdf4
Tom Graßmann <tom.grassmann@live.de>
2017-10-28 02:38:48 +0200

    AYYAAAY

--- src/obstacles/Blackhole.js
+++ src/obstacles/Blackhole.js
@@ -1,30 +1,25 @@
 function Blackhole(sprite) {
 
+    this.position = new Phaser.Point(sprite.x, sprite.y);
     this.sprite = sprite;
+
     this.sprite.prototype = {
         interact: function (player) {
 
-            var d = Object.create(Phaser.Point(this.x - player.x, this.y - player.y));
-            Console.log('vGravity: ' + d);
+            var d = new Phaser.Point(this.x - player.position.x, this.y - player.position.y);
+            console.log('vGravity: ' + d);
             var nd = normalize(d);
-            Console.log('vnGravity: ' + nd);
-            var gravity = Object.create(Phaser.Point(nd.x * player.speed, nd.y * player.speed));
+            console.log('vnGravity: ' + nd);
+            var gravity = new Phaser.Point(nd.x * 1.3, nd.y * 1.3);
 
-            player.direction.x = player.direction.x + gravity.x;
-            player.direction.y = player.direction.y + gravity.x;
+            player.velocity.x = gravity.x;
+            player.velocity.y = gravity.y;
         }
-    };
-
-
-    // Func. Interact
-    Blackhole.prototype.interact = function (player) {
-
-
     };
 
     function normalize(point) {
 
-        var len = Math.sqrt(point.x * point.x + point.y * point.y)
+        var len = Math.sqrt(point.x * point.x + point.y * point.y);
         point.x /= len;
         point.y /= len;
     }
