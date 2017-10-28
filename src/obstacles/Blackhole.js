c2b1ab1dee61a9cb90ab04db6d65351f6e71156a
Tom Graßmann <tom.grassmann@live.de>
2017-10-28 10:40:43 +0200

    fg5gwaergsxdfgfsdgh

--- src/obstacles/Blackhole.js
+++ src/obstacles/Blackhole.js
@@ -6,14 +6,15 @@ function Blackhole(sprite) {
     this.sprite.prototype = {
         interact: function (player) {
 
-            var d = new Phaser.Point(this.x - player.position.x, this.y - player.position.y);
-            console.log('vGravity: ' + d);
-            var nd = normalize(d);
-            console.log('vnGravity: ' + nd);
-            var gravity = new Phaser.Point(nd.x * 1.3, nd.y * 1.3);
-
-            player.velocity.x = gravity.x;
-            player.velocity.y = gravity.y;
+            // var d = new Phaser.Point(this.x - player.position.x, this.y - player.position.y);
+            // console.log('vGravity: ' + d);
+            // var nd = normalize(d);
+            // console.log('vnGravity: ' + nd);
+            // var gravity = new Phaser.Point(nd.x * 1.3, nd.y * 1.3);
+            //
+            // player.velocity.x = gravity.x;
+            // player.velocity.y = gravity.y;
+            console.log("interaction from blackhole");
         }
     };
 
@@ -23,6 +24,7 @@ function Blackhole(sprite) {
         point.x /= len;
         point.y /= len;
     }
+
     draw = function () {
 
     }
---
+++ src/obstacles/Player.js
@@ -0,0 +1,11 @@
+function Player(velocity, sprite){
+
+    this.position = new Phaser.Point(game.width / 2.0, game.height / 2.0);
+    this.velocity = velocity;
+    this.sprite = sprite;
+
+    this.update = function(world){
+
+        game.physics.arcade.overlap(sprite, cur_sprite, function() { cur_obstacle.interact(sprite) }, null, this);
+    }
+}
