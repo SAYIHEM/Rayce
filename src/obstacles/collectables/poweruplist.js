d20c6a17098149d8cdf32ea36ac5be9b9ab8d7d0
manuel-kr <snape27@web.de>
2017-10-29 14:34:36 +0100

    LÄUFT CHICH

--- src/obstacles/collectables/poweruplist.js
+++ src/obstacles/collectables/poweruplist.js
@@ -1,3 +1,4 @@
+/*
 function PowerUpList(specialFunc,game) {
     this.mSpecialFunc = specialFunc;
     this.mGame = game;
@@ -40,8 +41,6 @@ function PowerUpList(specialFunc,game) {
             this.mElements.add(sprite);
         }
 
-
-
     };
 
 
@@ -70,4 +69,83 @@ function PowerUpList(specialFunc,game) {
         curAmount = 0;
         //reset all sprides to empty
     };
+}
+*/
+
+class PowerUpList {
+
+    constructor(specialFunc,game) {
+        this.mSpecialFunc = specialFunc;
+        this.mGame = game;
+
+        this.MAX_ELEMENTS = 3;
+        this.curAmount = 0;
+
+        this.WIDTH_SCREEN_SCALE = 1/5;
+        this.MARGIN_TOP_SCALE = 1/10;
+        this.mElements = [];
+
+    }
+    show() {
+        this.mGame.load.image('powerup_e', 'assets/powerup_empty.png');
+        this.mGame.load.image('powerup_f', 'assets/powerup_fill.png');
+
+        var y = this.MARGIN_TOP_SCALE * this.mGame.camera.height;
+
+        for (var i=0;i<this.MAX_ELEMENTS;i++) {
+
+            var gameWidth = this.mGame.camera.width;
+
+            var listWidth = this.mGame.camera.width * this.WIDTH_SCREEN_SCALE;
+            var eachWidth = listWidth / this.MAX_ELEMENTS;
+
+            //console.log("listwidth " + listWidth);
+            //console.log("gamewidth " + gameWidth);
+            //console.log("eachWidth " + eachWidth);
+            //console.log("camera x " + this.mGame.camera.x + " y " + this.mGame.camera.y);
+            //console.log("camera width " + this.mGame.camera.width + " height " + this.mGame.camera.height);
+
+
+            var x = (gameWidth - listWidth) + (eachWidth*i);
+
+            var sprite = this.mGame.add.sprite(x, y, 'powerup_e');
+
+
+
+            sprite.bringToTop();
+
+            var scale = eachWidth/sprite.width;
+            sprite.scale.setTo(scale,scale);
+
+            sprite.fixedToCamera = true;
+            //sprite.cameraOffset.setTo(200, 500);
+
+            this.mElements.push(sprite);
+            //console.log("added mana " + x + " " + y + " " + sprite.width + " " + sprite.height);
+        }
+    }
+    addLoad() {
+        if (this.curAmount<this.MAX_ELEMENTS) {
+            //fill next powerup
+            var empty_sprite = this.mElements.get(this.curAmount);
+            var x = empty_sprite.body.x;
+            var y = empty_sprite.body.y;
+
+            var filled_sprite = this.mGame.add.sprite(x, y, 'powerup_f');
+
+            var cur_width = filled_sprite.body.width;
+            var scale = cur_width/empty_sprite.body.width;
+            filled_sprite.scale.setTo(scale,scale);
+
+            this.curAmount++;
+            if (this.curAmount===this.MAX_ELEMENTS) {
+                this.mSpecialFunc();
+            }
+        }
+    }
+
+    clear() {
+        this.curAmount = 0;
+        //reset all sprides to empty
+    }
 }
